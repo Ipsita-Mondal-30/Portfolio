91d127e6e2d295a5050a17917f69a1f96397b8be
Ipsita <ipsitaamondal@gmail.com>
2025-06-26 01:11:53 +0530

    fix: correct syntax for 'use client' directive in Experienceection.tsx

--- src/app/components/Experienceection.tsx
+++ src/app/components/Experienceection.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import React, { useState, useEffect, useRef } from 'react';
 import { Code2, Users, Briefcase,  Rocket, Palette, Globe, Award, Building } from 'lucide-react';
